eae6baf71e67c1c8763ccfa94fd09f99f46ef132
tekpixo <lincoln.mcosta@gmail.com>
2021-06-24 21:25:51 -0300

    feat: inserção do fast api.

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 from chatbot import chatbot
-from flask import Flask, render_template, request
-import os
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
-app = Flask(__name__)
-app.static_folder = 'static'
+from fastapi.templating import Jinja2Templates
 
-@app.route("/")
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    return str(chatbot.get_response(userText))
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-    
+    return templates.TemplateResponse("item.html")
