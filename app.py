f17da39f022c3fa15d21f2875a218238eff0067a
tekpixo <lincoln.mcosta@gmail.com>
2021-06-24 21:46:47 -0300

    feat: criação do endpoint de get da mensagem.

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/index",response_class=HTMLResponse)
+@app.get("/index", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.route("/get/{msg}")
+def get_bot_response(msg: str):
+    return str(chatbot.get_response(msg))
