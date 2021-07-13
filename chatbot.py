2b8857effc38dd4b763b74bc1aa1fb7f9c7375d6
fcarlosmonteiro <fcarlosmonteiro@gmail.com>
2021-07-13 14:12:45 -0300

    update dataset

--- chatbot.py
+++ chatbot.py
@@ -10,7 +10,7 @@ chatbot = ChatBot("Chatterbot", read_only=True)
 chatbot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter", logic_adapters=[{
         'import_path': 'chatterbot.logic.BestMatch',
         'statement_comparison_function': 'chatterbot.comparisons.levenshtein_distance',
-        'response_selection_method=get_most_frequent_response',
+        'response_selection_method':'get_most_frequent_response',
         'default_response': 'Não entendi, pode repetir por favor?',
         'maximum_similarity_threshold': 0.97}])
 
